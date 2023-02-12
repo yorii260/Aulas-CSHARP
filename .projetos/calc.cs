2b9f3879f481e973d03d6e9efaf31b3a06df626f
yorii <73965917+yorii260@users.noreply.github.com>
2023-02-12 10:43:07 -0300

    Mudanças.

--- .projetos/calc.cs
+++ .projetos/calc.cs
@@ -1,43 +1,46 @@
+
 using System;
 
-namespace Calculadora 
+namespace Calculadora
 {
     class Program
     {
         public static void Main()
         {
-            int[] notas = new int[10]; 
+            int[] notas = new int[10];
             string[] alunos = new string[3];
             int i = 0;
-            string aluno="";
-            int nota =0;
+            string aluno = "";
+            int nota = 0;
             int c = 0;
             bool t = true;
-            for(int x=0;x<=9;x++)
+            for (int x = 0; x <= 9; x++)
             {
-                if (c==4){
-                    t=true;
+                if (c == 4)
+                {
+                    t = true;
                 }
-                if(t == false){
-                    Console.Clear();                   
-                    Console.WriteLine("{0}\n\n\n",x);
-                    Console.WriteLine("Digite a {0}a nota do aluno {1}: ",c,alunos[i-1]);
+                if (t == false)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Digite a {0}. nota do aluno {1}: ", c, alunos[i - 1]);
                     nota = Convert.ToInt32(Console.ReadLine());
-                    notas[x-1]=nota;
+                    notas[x - 1] = nota;
                     c++;
 
                 }
-                if(
-                    x == 3 | x ==6 | x==10 | x==0 
+                if (
+                    x == 3 | x == 6 | x == 10 | x == 0
                 )
                 {
-                    Console.WriteLine("- Digite o nome do seu {0} aluno(a): ", i+1);
+                    Console.Clear();
+                    Console.WriteLine("- Digite o nome do seu {0}. aluno(a): ", i + 1);
                     aluno = Console.ReadLine();
-                    alunos[i]=aluno;
-                    c=1;
+                    alunos[i] = aluno;
+                    c = 1;
                     i++;
-                    t=false;
-                
+                    t = false;
+
                 }
 
 
@@ -46,7 +49,7 @@ namespace Calculadora
 
             Console.Clear();
 
-            Console.WriteLine("Eo resultado final foi o seguinte:\n\n- {0}: {1} | {2} | {3}\n{4}: {5} | {6} | {7}\n{8}: {9} | {10} | {11}",
+            Console.WriteLine("Eo resultado final foi o seguinte:\n\n- {0}:\n1. Nota: {1}\n2. Nota: {2}\n3. Nota: {3}\n\n{4}:\n1. Nota: {5}\n2. Nota: {6}\n3. Nota: {7}\n\n{8}:\n1 Nota: {9}\n2. Nota: {10}\n3. Nota: {11}",
             alunos[0],
 
             notas[0],
@@ -66,9 +69,9 @@ namespace Calculadora
             notas[8]
             );
 
-            int aluno1 = (notas[0]+notas[1]+notas[2])/3;
-            int aluno2 = (notas[3]+notas[4]+notas[5])/3;
-            int aluno3 = (notas[6]+notas[7]+notas[8])/3;
+            int aluno1 = (notas[0] + notas[1] + notas[2]) / 3;
+            int aluno2 = (notas[3] + notas[4] + notas[5]) / 3;
+            int aluno3 = (notas[6] + notas[7] + notas[8]) / 3;
 
             int Aprovado = 6;
             int Reprovado = 3;
@@ -77,11 +80,11 @@ namespace Calculadora
             string aluno1_status = "";
             string aluno2_status = "";
             string aluno3_status = "";
-            if (aluno1 >= Aprovado ){aluno1_status="Aprovado";}else if (aluno1 <= Rec){aluno1_status="Recuperação";}else if(aluno1<=Reprovado){aluno1_status="Reprovado";}
-            if (aluno2 >= Aprovado ){aluno2_status="Aprovado";}else if (aluno2 <= Rec){aluno2_status="Recuperação";}else if(aluno2<=Reprovado){aluno2_status="Reprovado";}
-            if (aluno3 >= Aprovado ){aluno3_status="Aprovado";}else if (aluno3 <= Rec){aluno3_status="Recuperação";}else if(aluno3<=Reprovado){aluno3_status="Reprovado";}
+            if (aluno1 >= Aprovado) { aluno1_status = "Aprovado"; } else if (aluno1 <= Rec) { aluno1_status = "Recuperação"; } else if (aluno1 <= Reprovado) { aluno1_status = "Reprovado"; }
+            if (aluno2 >= Aprovado) { aluno2_status = "Aprovado"; } else if (aluno2 <= Rec) { aluno2_status = "Recuperação"; } else if (aluno2 <= Reprovado) { aluno2_status = "Reprovado"; }
+            if (aluno3 >= Aprovado) { aluno3_status = "Aprovado"; } else if (aluno3 <= Rec) { aluno3_status = "Recuperação"; } else if (aluno3 <= Reprovado) { aluno3_status = "Reprovado"; }
 
-            Console.WriteLine("\n\nAs n0tas finais dos respectivos alunos foram:\n{0}: {1} | {2}\n{3}: {4} | {5}\n{6}: {7} | {8}",
+            Console.WriteLine("\n\nAs notas finais dos respectivos alunos foram:\n{0}: {1} | {2}\n{3}: {4} | {5}\n{6}: {7} | {8}",
 
             alunos[0],
             aluno1,
---
+++ 33/aula33.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Curso
+{
+    public class Aula33
+    {
+        static public void Main()
+        {
+            Console.WriteLine("Olá mundo!");
+        }
+    }
+}
